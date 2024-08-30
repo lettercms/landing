9b75b389989923041843fad9a7cd4709ff4b1cb6
davidsdevel <davidsdevel@gmail.com>
2024-08-30 02:12:49 +0000

    build: Add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,47 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			logo: {
-				light: './src/assets/lettercms-logo.svg',
-				dark: './src/assets/lettercms-logo-white.svg'
-			},
-			defaultLocale: 'root',
-			locales: {
-				es: {
-					label: 'Español',
-					lang: 'es'
-				},
-				root: {
-					label: 'English',
-					lang: 'en'
-				}
-			},
-			title: 'LetterCMS',
-			social: {
-				github: 'https://github.com/lettercms/core'
-			},
-			customCss: [
-				'./src/tailwind.css',
-			],
-			sidebar: [
-				{slug: 'docs'},
-				{slug: 'docs/getting-started'},
-				{slug: 'docs/architecture'},
-				{slug: 'docs/usage'},
-				{
-					label: 'Contributing',
-					items: [
-					  'docs/contributing/intro',
-					  'docs/contributing/guidelines',
-					],
-				},
-				{slug: 'docs/roadmap'},
-			]
-		}),
-		tailwind({})
-	]
+  integrations: [starlight({
+    logo: {
+      light: './src/assets/lettercms-logo.svg',
+      dark: './src/assets/lettercms-logo-white.svg'
+    },
+    defaultLocale: 'root',
+    locales: {
+      es: {
+        label: 'Español',
+        lang: 'es'
+      },
+      root: {
+        label: 'English',
+        lang: 'en'
+      }
+    },
+    title: 'LetterCMS',
+    social: {
+      github: 'https://github.com/lettercms/core'
+    },
+    customCss: ['./src/tailwind.css'],
+    sidebar: [{
+      slug: 'docs'
+    }, {
+      slug: 'docs/getting-started'
+    }, {
+      slug: 'docs/architecture'
+    }, {
+      slug: 'docs/usage'
+    }, {
+      label: 'Contributing',
+      items: ['docs/contributing/intro', 'docs/contributing/guidelines']
+    }, {
+      slug: 'docs/roadmap'
+    }]
+  }), tailwind({})],
+  output: "server",
+  adapter: vercel()
 });
