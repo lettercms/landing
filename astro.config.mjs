6f529354ba55aed87f25c25b616bab3121170bcf
davidsdevel <davidsdevel@gmail.com>
2025-01-04 21:37:36 +0000

    chore

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,42 +6,42 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    logo: {
-      light: './src/assets/lettercms-logo.svg',
-      dark: './src/assets/lettercms-logo-white.svg'
-    },
-    defaultLocale: 'root',
-    locales: {
-      es: {
-        label: 'Español',
-        lang: 'es'
+  integrations: [
+    starlight({
+      logo: {
+        light: './src/assets/lettercms-logo.svg',
+        dark: './src/assets/lettercms-logo-white.svg'
       },
-      root: {
-        label: 'English',
-        lang: 'en'
-      }
-    },
-    title: 'LetterCMS',
-    social: {
-      github: 'https://github.com/lettercms/core'
-    },
-    customCss: ['./src/tailwind.css'],
-    sidebar: [{
-      slug: 'docs'
-    }, {
-      slug: 'docs/getting-started'
-    }, {
-      slug: 'docs/architecture'
-    }, {
-      slug: 'docs/usage'
-    }, {
-      label: 'Contributing',
-      items: ['docs/contributing/intro', 'docs/contributing/guidelines']
-    }, {
-      slug: 'docs/roadmap'
-    }]
-  }), tailwind({})],
+      defaultLocale: 'root',
+      locales: {
+        es: {
+          label: 'Español',
+          lang: 'es'
+        },
+        root: {
+          label: 'English',
+          lang: 'en'
+        }
+      },
+      title: 'LetterCMS',
+      social: {
+        github: 'https://github.com/lettercms/core'
+      },
+      customCss: ['./src/tailwind.css'],
+      sidebar: [
+        'docs',
+        'docs/getting-started',
+        'docs/architecture',
+        'docs/usage',
+        {
+          label: 'Contributing',
+          items: ['docs/contributing/intro', 'docs/contributing/guidelines']
+        },
+        'docs/roadmap'
+      ]
+    }),
+    tailwind({})
+  ],
   output: "server",
   adapter: vercel()
 });
